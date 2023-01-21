75fcd4589652046d90644d1c408d129a0594c53b
LAPTOP-RHE7OVKJ\dipti <20BCS5145@cuchd.in>
2023-01-21 10:34:06 +0530

    Initial Commit

---
+++ client/src/components/ChannelSearch.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect} from 'react';
+import { useChatContext } from 'stream-chat-react';
+
+import {SearchIcon} from '../assets';
+
+const ChannelSearch = () => {
+  return (
+    <div className='channel-search__container'>
+        <div className='channel-search__input__wrapper'></div>
+        
+    </div>
+  )
+}
+
+export default ChannelSearch
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+//whole application will be inside this component and in root div
+ReactDOM.render(<App />, document.getElementById('root'));
+
