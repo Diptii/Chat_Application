2fae64e40d9076d9d02869c9bc51f9292ae3a9d5
Priyansh <priyanshkumar001@gmail.com>
2023-01-23 12:42:59 +0530

    Signin Signup Page Frontend Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { StreamChat } from 'stream-chat';
-import { Chat } from 'stream-chat-react';
-import Cookies from 'universal-cookie';
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
-import { ChannelListContainer, ChannelContainer} from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 
-import './App.css';
+import "./App.css";
 
-const apikey = '4h9w5naydhk3';
+const apikey = "4h9w5naydhk3";
 
 const client = StreamChat.getInstance(apikey);
 
-const app = () => {
-  return (
-    <div className='app__wrapper'>
-      <Chat client={client} theme='team light'>
-          <ChannelListContainer
-            
-          />
-          <ChannelContainer
+const authToken = false;
 
-          />
+const app = () => {
+  if (!authToken) return <Auth />;
+
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
       </Chat>
     </div>
   );
-}
+};
 
-export default app
+export default app;
